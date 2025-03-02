f56403dd259072d448f8c654597e99bdf377f242
Davimendonca7 <davi.mendonca@sptech.school>
2025-03-02 14:05:11 -0300

    Entidade endereço desenvolvida

---
+++ servico-usuario/src/main/java/com/streetaparrel/servico_usuario/servico_usuario/domain/endereco/EnderecoRepository.java
@@ -0,0 +1,8 @@
+package com.streetaparrel.servico_usuario.servico_usuario.domain.endereco;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
